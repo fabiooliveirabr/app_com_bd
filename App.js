a4b37b54d6f3b04a58576e3bd40b68fa129d5b8c
fabiooliveirabr <136032550+fabiooliveirabr@users.noreply.github.com>
2024-06-12 20:54:32 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { supabase } from './conexao';
 
 export default function App() {
-  const [descricaoDigitada, setDescricaoDigitada] = useState("");
-  const [valorDigitado, setValorDigitado] = useState("");
-  // Criar uma função para inserir no Banco de dados
-  const cadastrarConta = async(desc, vl)=>{
-    const {error} = await supabase.from("tb_contas")
-    .insert({coluna_descricao: desc, coluna_valor: vl, coluna_status: false});
-
-    if(error){
-      alert("Falha ao cadastrar!")
-    }else{
-      alert("Cadastrado com sucesso!")
-    }
-
-  }
-
   return (
     <View style={styles.container}>
       
@@ -28,6 +11,7 @@ export default function App() {
         placeholder='Descrição aqui'
         onChangeText={(texto)=>setDescricaoDigitada(texto)}
       />
+
       <TextInput
         style={styles.caixaDeTexto}
         placeholder='Valor aqui'
@@ -37,14 +21,33 @@ export default function App() {
         title="Cadastrar"
         onPress={()=>{cadastrarConta(descricaoDigitada, valorDigitado)}}
       />
-
+      <ScrollView style={{width:"100%"}}>
+        {/* Mapear os dados e dividir em itens */}
+        {dados.map((item)=>(
+            <View style={styles.caixaContas}>
+              <Text>{item.coluna_descricao}</Text>
+              <Text>R$ {item.coluna_valor}</Text>
+              <Text> {item.coluna_status} </Text>
+            </View>
+        ))}
+      </ScrollView>
+      
 
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
+  caixaContas:{
+      width: "90%",
+      minHeight: 70,
+      borderWidth: 1,
+      borderColor: "#b2b2b2",
+      borderRadius: 8,
+      margin: "auto",
+      marginTop: 8,
+      padding: 10
+  },
   caixaDeTexto:{
     borderWidth: 1,
     width: "90%",
