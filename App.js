f278e71efe02914d3d22af754ddca6dc09a3d3a7
Fábio Oliveira <fabiodeoliveirabr@gmail.com>
2024-06-12 19:30:28 -0300

    19h30 aula 12/06

--- App.js
+++ App.js
@@ -1,7 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { supabase } from './conexao';
 
 export default function App() {
+  const [descricaoDigitada, setDescricaoDigitada] = useState("");
+  const [valorDigitado, setValorDigitado] = useState("");
+  const [dados, setDados] = useState([]);
+  //Função para consultar os dados no Banco de Dados
+  const consultarDados = async()=>{
+      const {data, error} = await supabase.from("tb_contas")
+      .select("*");
+    if(error){
+        alert("Falha ao consultar os dados!")
+    }else{
+        setDados(data);
+    }
+
+  }  
+
+  // Criar uma função para inserir no Banco de dados
+  const cadastrarConta = async(desc, vl)=>{
+    const {error} = await supabase.from("tb_contas")
+    .insert({coluna_descricao: desc, coluna_valor: vl, coluna_status: false});
+
+    if(error){
+      alert("Falha ao cadastrar!")
+    }else{
+      alert("Cadastrado com sucesso!")
+    }
+
+  }
+
   return (
     <View style={styles.container}>
       
@@ -9,14 +39,17 @@ export default function App() {
       <TextInput
         style={styles.caixaDeTexto}
         placeholder='Descrição aqui'
+        onChangeText={(texto)=>setDescricaoDigitada(texto)}
       />
+
       <TextInput
         style={styles.caixaDeTexto}
         placeholder='Valor aqui'
+        onChangeText={(texto)=>setValorDigitado(texto)}
       />
       <Button
         title="Cadastrar"
-        onPress={()=>{}}
+        onPress={()=>{cadastrarConta(descricaoDigitada, valorDigitado)}}
       />
 
 
